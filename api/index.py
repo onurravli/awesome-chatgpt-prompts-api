a0f932e563c1f4bc139dc3c9e3178ba08b5dd751
onurravli <onurravli@hotmail.com>
2023-03-30 16:56:18 +0300

    Update files

--- api/index.py
+++ api/index.py
@@ -1,11 +1,23 @@
 from flask import Flask
+import json
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/")
+def home():
+    return {"available_routes": "/act/:act"}
+
+
+@app.route("/act/<string:act>")
+def act(act: str):
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "../static", "prompts.json")
+    data = json.load(open(json_url))
+    for i in range(len(data)):
+        if data[i]["act"].lower() == act.lower():
+            resp = data[i]["prompt"]
+            ret = {"act": act.lower(), "prompt": resp}
+            return ret
+    return {"error": "Prompt doesn't exist."}
